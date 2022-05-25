29162d022372cc89b67f7123592c611a2f956a9f
aokik-ao <61224695+aokik-ao@users.noreply.github.com>
2022-05-25 12:38:04 +0900

    顕現機能の追加

--- src/components/organisms/user/UserDetailModal.tsx
+++ src/components/organisms/user/UserDetailModal.tsx
@@ -6,22 +6,48 @@ import {
   ModalBody,
   ModalCloseButton,
   ModalContent,
+  ModalFooter,
   ModalHeader,
   ModalOverlay,
   Stack
 } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { ChangeEvent, memo, useEffect, useState, VFC } from "react";
 
 import { User } from "../../../types/api/user";
+import { PrimaryButton } from "../../atoms/button/PrimaryButton";
 
 type Props = {
   user: User | null;
   isOpen: boolean;
+  isAdmin?: boolean;
   onClose: () => void;
 };
 
 export const UserDetailModal: VFC<Props> = memo((props) => {
-  const { user, isOpen, onClose } = props;
+  const { user, isOpen, isAdmin = false, onClose } = props;
+
+  const [userName, setUserName] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  useEffect(() => {
+    setUserName(user?.username ?? "");
+    setName(user?.name ?? "");
+    setEmail(user?.email ?? "");
+    setPhone(user?.phone ?? "");
+  }, [user]);
+
+  const onChanegeUserName = (e: ChangeEvent<HTMLInputElement>) =>
+    setUserName(e.target.value);
+  const onChanegeName = (e: ChangeEvent<HTMLInputElement>) =>
+    setName(e.target.value);
+  const onChanegeEmail = (e: ChangeEvent<HTMLInputElement>) =>
+    setEmail(e.target.value);
+  const onChanegePhone = (e: ChangeEvent<HTMLInputElement>) =>
+    setPhone(e.target.value);
+
+  const onClickUpdate = () => alert();
   return (
     <Modal
       isOpen={isOpen}
@@ -30,29 +56,50 @@ export const UserDetailModal: VFC<Props> = memo((props) => {
       motionPreset="slideInBottom"
     >
       <ModalOverlay />
-      <ModalContent pb={6}>
+      <ModalContent pb={2}>
         <ModalHeader>ユーザー詳細</ModalHeader>
         <ModalCloseButton />
         <ModalBody mx={4}>
           <Stack spacing={4}>
             <FormControl>
               <FormLabel>名前</FormLabel>
-              <Input value={user?.username} isReadOnly />
+              <Input
+                value={userName}
+                isReadOnly={!isAdmin}
+                onChange={onChanegeUserName}
+              />
             </FormControl>
             <FormControl>
               <FormLabel>フルネーム</FormLabel>
-              <Input value={user?.name} isReadOnly />
+              <Input
+                value={name}
+                isReadOnly={!isAdmin}
+                onChange={onChanegeName}
+              />
             </FormControl>
             <FormControl>
               <FormLabel>MAIL</FormLabel>
-              <Input value={user?.email} isReadOnly />
+              <Input
+                value={email}
+                isReadOnly={!isAdmin}
+                onChange={onChanegeEmail}
+              />
             </FormControl>
             <FormControl>
               <FormLabel>TEL</FormLabel>
-              <Input value={user?.phone} isReadOnly />
+              <Input
+                value={phone}
+                isReadOnly={!isAdmin}
+                onChange={onChanegePhone}
+              />
             </FormControl>
           </Stack>
         </ModalBody>
+        {isAdmin && (
+          <ModalFooter>
+            <PrimaryButton onClick={onClickUpdate}>更新</PrimaryButton>
+          </ModalFooter>
+        )}
       </ModalContent>
     </Modal>
   );
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -19,6 +19,7 @@ export const UserManagement: VFC = memo(() => {
   const { getUsers, users, loading } = useAllUsers();
   const { onSelectUser, selectedUser } = useSelectUser();
   const { loginUser } = useLoginUser();
+  console.log(loginUser);
 
   useEffect(() => getUsers(), []);
 
@@ -51,7 +52,12 @@ export const UserManagement: VFC = memo(() => {
           ))}
         </Wrap>
       )}
-      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal
+        user={selectedUser}
+        isOpen={isOpen}
+        isAdmin={loginUser?.isAdmin}
+        onClose={onClose}
+      />
     </>
   );
 });
